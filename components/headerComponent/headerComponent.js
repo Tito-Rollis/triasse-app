71a70b0645351cfe38bdec7205d47a7d2319228f
Tito-Rollis <ouvalres@gmail.com>
2022-01-06 22:32:06 +0700

    header done, not responsive yet

---
+++ components/headerComponent/headerComponent.js
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+
+export default function HeaderComponent() {
+    return (
+        <div className="w-screen flex flex-wrap ">
+            {/* FIRST HEADER */}
+            <div className="py-3  px-20 flex justify-end gap-8  bg-grey-100 w-full">
+                <div className="flex gap-1 items-center">
+                    <Image
+                        src="/img/img-calendar.svg"
+                        alt="calendar"
+                        width="15.833"
+                        height="16.225"
+                    />
+                    <p className="text-p text-grey-300">
+                        Senin - Jumat &#40;09.00 - 18.00 WIB &#41;
+                    </p>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <Image
+                        src="/img/img-message.svg"
+                        alt="message"
+                        width="22.007"
+                        height="15.656"
+                    />
+                    <p className="text-p text-grey-300">+62811 1122 015</p>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <Image
+                        src="/img/img-email.svg"
+                        alt="message"
+                        width="19.239"
+                        height="13.158"
+                    />
+                    <p className="text-p text-grey-300">cs@triasse.com</p>
+                </div>
+            </div>
+            {/* SECOND HEADER */}
+            <div className="py-3 px-20 flex w-full justify-between items-center">
+                <Image
+                    src="/img/img-brand-logo.png"
+                    alt="Logo"
+                    width="100"
+                    height="51"
+                />
+                <div className="flex gap-x-8 items-center">
+                    {/* LINK HEADER */}
+                    <div className="flex gap-6">
+                        <h1 className="text-subTitle text-blue-200">
+                            Paket Test Darah
+                        </h1>
+                        <h1 className="text-subTitle text-blue-200">
+                            Laboratorium
+                        </h1>
+                        <h1 className="text-subTitle text-blue-200">Artikel</h1>
+                    </div>
+                    {/* BUTTON HEADER */}
+                    <div className="flex gap-x-5">
+                        <button className="py-2 px-6 rounded-sm bg-whiteBlue">
+                            <p className="text-blue-100 font-medium">Masuk</p>
+                        </button>
+                        <button className="py-2 px-6 rounded-sm bg-blue-100">
+                            <p className="text-white font-medium">Daftar</p>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
         './components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
+        colors: {
+            white: '#FFF',
+            grey: { 100: '#F6FBFF', 200: '#F4F6F7', 300: '#686B7F' },
+            whiteBlue: '#EFF7FF',
+            blue: { 100: '#208AE5', 200: '#0A315F' },
+            orange: '#FF7900',
+        },
+        fontSize: {
+            title: '20px',
+            subTitle: '14px',
+            p: '12px',
+        },
         extend: {},
     },
     plugins: [],
